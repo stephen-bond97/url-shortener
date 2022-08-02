f6b54e2ded9d00256dff8c83e1050ca4415a85a4
Stephen <denial1997@hotmail.co.uk>
2022-08-02 13:55:23 +0100

    Adding Environment Variable to Heroku & adding connection string

--- Models/entitycoreContext.cs
+++ Models/entitycoreContext.cs
@@ -22,8 +22,11 @@ namespace UrlShortener.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Database=entitycore;Username=postgres;Password=su_root");
+                var connectionString = Environment.GetEnvironmentVariable("pgsql_connection_string");
+
+                //optionsBuilder.UseNpgsql("Host=localhost;Database=entitycore;Username=postgres;Password=su_root");
+                optionsBuilder.UseNpgsql(connectionString);
+
             }
         }
 
@@ -31,7 +34,7 @@ namespace UrlShortener.Models
         {
             modelBuilder.Entity<Item>(entity =>
             {
-                entity.ToTable("item", "account");
+                entity.ToTable("item");
 
                 entity.Property(e => e.Id)
                     .HasColumnName("id")
