a43baa152d3e875625e8a5871ea1d8ba41f2b71c
Stephen <denial1997@hotmail.co.uk>
2022-08-03 12:56:24 +0100

    Adding Cors

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,9 +1,20 @@
 using UrlShortener.Data;
 using UrlShortener.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var modelBuilder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+modelBuilder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://stephen-bond97.github.io");
+                      });
+});
 
 modelBuilder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,5 +37,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
